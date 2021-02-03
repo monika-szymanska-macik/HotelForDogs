ac3134d8ba58a83a8ed6cdb1a38e65189d4af2eb
Monika <m.szymanska.macik@gmail.com>
2021-02-03 18:54:08 +0100

    method to count how many days dog would spend in the hotel is added

--- HotelForDogs/HotelForDogs/Program.cs
+++ HotelForDogs/HotelForDogs/Program.cs
@@ -6,7 +6,27 @@ namespace HotelForDogs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("I invite you to visit my Hotel for Dogs! You can spend your holidays not worrying about your dog.");
+            Console.WriteLine("Please enter the day of arrival of your dog: ");
+            Console.WriteLine("(for example: 31-01-2021)");
+
+            string startDay = Console.ReadLine();
+            DateTime firstDay;
+            DateTime.TryParse(startDay, out firstDay);
+
+            Console.WriteLine("Please enter the day of departure of your dog");
+
+            string dayEnd = Console.ReadLine();
+            DateTime lastDay;
+            DateTime.TryParse(dayEnd, out lastDay);
+            Dog dog = new SmallDog(1, "Fiona", "Sznaucer", 8);
+
+            Client clien = new Client(1, "Monika", "Monika", "55");
+
+            StayAtHotel stay = new StayAtHotel(1, clien, 50, dog);
+
+            stay.DayNumberOfStay(firstDay, lastDay);
+
         }
     }
 }
---
+++ HotelForDogs/HotelForDogs/StayAtHotel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelForDogs
+{
+    class StayAtHotel
+    {
+        public StayAtHotel(int id, Client client, decimal price, Dog dog)
+        {
+            Id = id;
+            Client = client;
+            Price = price;
+            Dog = dog;
+        }
+
+        public int Id { get; set; }
+        public Client Client { get; set; }
+        public decimal Price { get; set; }
+        public Dog Dog { get; set; }
+        public int TimeOfStayAtHotel { get; set; }
+
+        public string DayNumberOfStay(DateTime firstDay, DateTime lastDay)
+        {
+            TimeSpan result = lastDay - firstDay;
+            string TotalDays = result.TotalDays.ToString();
+            Console.WriteLine(TotalDays);
+            return TotalDays;
+        }
+    }
+}
