5fadff7a76a356f9a96b1ebf4a43389bf8ebabee
Monika <m.szymanska.macik@gmail.com>
2021-02-14 07:21:36 +0100

    methode HpwLongStayInHotel added

--- HotelForDogs/HotelForDogs/StayAtHotel.cs
+++ HotelForDogs/HotelForDogs/StayAtHotel.cs
@@ -29,51 +29,26 @@ namespace HotelForDogs
             var account = _clientAccount.GetClientAccountById(clientId);
             Console.WriteLine($"This is an account of {account.FirstName} {account.LastName}.");
             Console.WriteLine($"Your dog is {account.Dog.Name}");
-            Console.WriteLine("Please enter the day of arrival of your dog: ");
-            Console.WriteLine("(for example: 31-01-2021)");
 
-            string startDay = Console.ReadLine();
-            DateTime firstDay;
-            DateTime.TryParse(startDay, out firstDay);
+            _clientAccount.HowLongIsStay(account.Dog);
 
-            Console.WriteLine("Please enter the day of departure of your dog");
-
-            string dayEnd = Console.ReadLine();
-            DateTime lastDay;
-            DateTime.TryParse(dayEnd, out lastDay);
-            listOfStays.Add(stay);
-            int lengthOfStay = DayNumberOfStay(firstDay, lastDay);
-            Console.WriteLine($"Your dog will stay with us for {lengthOfStay} days");
-            decimal price = Price(lengthOfStay, account.Dog.Weight);
-            Console.WriteLine($"The cost of the stay is {price} PLN");
             listOfStays.Add(stay);        
 
         }
         public void AddNewStayForNewClient()
         {
             StayAtHotel stay = new StayAtHotel();
-            
+
             var dog = _clientAccount.CreateDogAccount();
             _clientAccount.CreateClientAccount(dog);
-            Console.WriteLine("Please enter the day of arrival of your dog: ");
-            Console.WriteLine("(for example: 31-01-2021)");
 
-            string startDay = Console.ReadLine();
-            DateTime firstDay;
-            DateTime.TryParse(startDay, out firstDay);
-
-            Console.WriteLine("Please enter the day of departure of your dog");
-
-            string dayEnd = Console.ReadLine();
-            DateTime lastDay;
-            DateTime.TryParse(dayEnd, out lastDay);          
-            int lengthOfStay = DayNumberOfStay(firstDay, lastDay);
-            Console.WriteLine($"Your dog will stay with us for {lengthOfStay} days");
-            decimal price = Price(lengthOfStay, dog.Weight);
-            Console.WriteLine($"The cost of a {dog.Name} stay is {price} PLN");
+            _clientAccount.HowLongIsStay(dog);
             listOfStays.Add(stay);
 
         }
+
+        
+
         public List<StayAtHotel> GetStayList()
         {
             return listOfStays;
@@ -91,43 +66,7 @@ namespace HotelForDogs
             }
             return clientStay;
         }
-        private int DayNumberOfStay(DateTime firstDay, DateTime lastDay)
-        {
-            TimeSpan result = lastDay - firstDay;
-            string TotalDays = result.TotalDays.ToString();
-            int totalDays;
-            Int32.TryParse(TotalDays, out totalDays);
-            return totalDays;
-        }
-        private decimal Price(int totalDays, int weight)
-        {
-            decimal price;
-
-
-            if (weight <= 10)
-            { 
-                if (totalDays > 3)
-                {
-                    price = totalDays * 50;
-                }
-                else
-                {
-                    price = totalDays * 60;
-                }
-            }
-            else
-            {
-                if (totalDays > 3)
-                {
-                    price = totalDays * 60;
-                }
-                else
-                {
-                    price = totalDays * 70;
-                }
-            }
-            return price;
-        }
+        
 
         
         
